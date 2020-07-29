7e1cb1932426f90cf509905aaf669d88c793c9d0
chao.yang <chao.yang.phoenixstudio@puzzleplusgames.com>
2020-07-29 19:32:14 +0800

    增加rust日志系统

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 
 mod engine;
-use crate::engine::input;
+use engine::input;
+use log;
+use fern;
+use chrono;
+
 fn vec_test(){
     let mut vec = engine::vector2::Vector2::new(10.0, 20.0);
     println!("{}, length {} ", vec, vec.length());
@@ -19,9 +23,29 @@ fn matrix_test(){
     println!("{}", a * a);
 }
 
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}][{}:{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.level(),
+                record.target(),
+                record.file().unwrap(),
+                record.line().unwrap(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply()?;
+    Ok(())
+}
+
 fn main() {
     //vec_test();
     //matrix_test();
+    setup_logger();
     let mut game = engine::game::Game::new();
     game.start();
     game.run();
